877f5f2526c6214fac0d6b165863d27b7ffbb074
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-05-09 10:55:24 -0400

    fasegsrt

---
+++ Tyler Ovenden New Project/src/introduction/CloseableFrame.java
@@ -0,0 +1,45 @@
+// Closable Frame Class
+// Written by: Craig A. Lindley
+// Last Update: 08/02/98
+
+
+package introduction;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class CloseableFrame extends Frame {
+
+	public CloseableFrame(String title) {
+		super(title);
+
+		// Add window listener, which allows Frame window to close
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				windowIsClosing();
+			}
+		});
+	}
+
+	// Register listener
+	public void registerCloseListener(CloseableFrameIF closeableIF) {
+
+		this.closeableIF = closeableIF;
+	}
+	
+	
+	// Called on close event
+	public void windowIsClosing() {
+
+		// Do call back if appropriate
+		if (closeableIF != null)
+			closeableIF.windowClosing();
+		
+		// Close the frame down
+		dispose();
+	}
+
+	// Private class data
+	private CloseableFrameIF closeableIF = null;
+}
+
---
+++ Tyler Ovenden New Project/src/introduction/CloseableFrameIF.java
@@ -0,0 +1,13 @@
+// Closable Frame Interface 
+// Written by: Craig A. Lindley
+// Last Update: 08/02/98
+
+package introduction;
+
+import java.awt.*;
+
+public interface CloseableFrameIF {
+
+	// Method called as Frame window is closing down
+	public void windowClosing();
+}
